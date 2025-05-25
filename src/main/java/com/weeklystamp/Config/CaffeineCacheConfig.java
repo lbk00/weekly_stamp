bed5826090028426f36f3bf7cec3c36ded8e41d1
lbk00 <lbk6661@gmail.com>
2025-05-25 17:26:51 +0900

    [FEAT] : 캐시 매니저 추가

---
+++ src/main/java/com/weeklystamp/Config/CaffeineCacheConfig.java
@@ -0,0 +1,30 @@
+package com.weeklystamp.Config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CaffeineCacheConfig {
+
+    @Bean
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(30, TimeUnit.MINUTES)
+                .maximumSize(10_000);
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine<Object, Object> caffeine) {
+        CaffeineCacheManager manager = new CaffeineCacheManager("checkInCache");
+        manager.setCaffeine(caffeine);
+        return manager;
+    }
+}
+
